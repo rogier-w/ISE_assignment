4f225012b9bfdebc262e997f144d2145c125300b
juliusj03 <116172471+juliusj03@users.noreply.github.com>
2022-12-25 12:42:59 +0100

    Update Server.py

--- Assignment/Server/Server.py
+++ Assignment/Server/Server.py
@@ -1,29 +1,81 @@
-# Author information
-# ...
-#
-#
-#
-
 import socket
+import threading
+import hashlib
 
-#Specification 1 Establish a connection to the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
-# Get the server's IP address and port number
-server_host = "123.456.789.0" 
-server_port = 1234  
 
-s.bind((server_host, server_port))
-s.listen()
-connection, client_address = s.accept()
-print(f"Connected by {client_adress}")
-while True:
-    data = connection.recv(1024)
-    if data:
-        print("Sending data to client")
-        connection.sendall(data)
-    if not data:
-        print("No data from {client_adress}")
-        break
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connected = True
+    login(conn)
 
-connection.close
+    while connected:
+
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+
+        if msg_length:
+
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            print(f"[{addr}] {msg}")
+            msgback = 'received message'
+            if '.!' in msg:
+                msgback = checkuserandpass(msg)
+                conn.send(msgback.encode(FORMAT))
+                if msgback == 'successfully logged in'or msgback == 'account made':
+                    print('worked')
+                    conn.close()
+                    break
+
+            elif msgback == DISCONNECT_MESSAGE:
+                connected = False
+                conn.close()
+
+
+def login(conn):
+    conn.send("""please login to continue, if you already have an account press 1 if you want to make a new account press 0 """
+              .encode(FORMAT))
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print("\n")
+        print(f"[ACTIVE CONNECTION] {threading.active_count() - 1}")
+def checkuserandpass(userandpass):
+    userpasslist = userandpass.split('.!')
+    userpassword = userpasslist[0]
+    code = userpasslist[1]
+    if code == '0':
+        with open('Credentials.txt', 'a') as f:
+            f.write(userpassword + '\n')
+            f.close()
+            return 'account made'
+
+
+
+    else:
+        with open('Credentials.txt', 'r') as f:
+            for line in f:
+                if userpassword in line:
+                    f.close()
+                    return 'successfully logged in'
+            f.close()
+            return 'invalid credentials'
+
+
+
+
+print('server starting')
+start()
